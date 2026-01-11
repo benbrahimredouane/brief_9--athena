1d626c13d958de6de57b024b04cd28f49626c3aa
redouane <benbrahimredouane664@gmail.com>
2026-01-11 19:53:28 +0100

    feat: error page template for exception handling

---
+++ views/error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <h1>Error</h1>
+    <p><?php echo $error; ?></p>
+    <a href="index.php">Go back to home</a>
+</body>
+</html>
