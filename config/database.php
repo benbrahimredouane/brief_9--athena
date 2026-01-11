62618ad5637b042128030c94e11e35ac6afc0deb
redouane <benbrahimredouane664@gmail.com>
2026-01-11 19:44:35 +0100

    feat: database connection class with PDO configuration

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $dbname = "athena";
+    private $username = "root";
+    private $password = "123456789";
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->dbname,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            throw new DatabaseException("Connection failed: " . $e->getMessage());
+        }
+        return $this->conn;
+    }
+}
+
+class DatabaseException extends Exception {}
